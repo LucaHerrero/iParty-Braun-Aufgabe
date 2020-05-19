ae197c8c5aff6a10296e72d8ed3fc3ef32f6aaa9
Luca Herrero <lucaherreromarcos@gmail.com>
2020-05-19 13:25:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Kurzgesagt das Apache oder NGINX in NodeJS.
 const express = require('express');
 const app = express();
-// Damit haben wir zugriff auf Lokale Datein auf Ihrem PC.
+// Damit haben wir zugriff auf lokale Datein auf Ihrem PC.
 const fs = require('fs');
-// Dieses Packet dient einzig und allein dazu einen Link in ihrem Browser zu öffnen.
+// Dieses Packet dient einzig und allein dazu einen Link in Ihrem Browser zu öffnen.
 const open = require('open');
-// Dieses Paket dient dazu, falls das Open Paket versagt, dass Sie in ihrem Terminal auf ein Link klicken können...
+// Dieses Paket dient dazu, falls das "open" Paket versagt, dass Sie in Ihrem Terminal auf ein Link klicken können...
 // Und nicht wie Steinzeit Menschen es mühsam abtippen müssen.
 const terminalLink = require('terminal-link');
 
@@ -15,15 +15,16 @@ app.use('/', express.static(__dirname + '/src'));
 app.use('/js', express.static(__dirname + '/node_modules/framework7/js'));
 app.use('/css', express.static(__dirname + '/node_modules/framework7/css'));
 
-// Sagt Ihrenen Apache .htaccess -> "DirectoryIndex" etwas? Genau so kann man sich das etwa vorstellen.
+// Sagt Ihnen Apache .htaccess -> "DirectoryIndex" etwas? Genau so kann man sich das in etwa vorstellen.
 // Man kann halt nicht existente Unterverzeichnisse verlinken (so funktionieren auch diese "costum" 404 Seiten).
-// Im ersten Abschnitt müssen wir jedoch jede Seite definieren die eigenes nicht existente Unterverzeichis haben soll.
-// In diesem Beispiel benutzt für eine Funkrion für das versenden von Mails.
+// Im ersten Abschnitt müssen wir jedoch jede Seite definieren die ein eigenes nicht existente Unterverzeichis haben soll.
+// In diesem Beispiel benutzt für eine Funktion, für das versenden von Mails.
 app.get('/test', function (req, res) {
     res.send('Hello World!');
 });
 // Hier werden alle Unterverzeichnisse, außer das oben genannte auf die Main index.html datei verlinkt, egal was sie aufrufen Sie kommen immer nur zur Startseite...
 // Kurzes Beispiel rufen Sie "http://localhost:666/ich-liebe-kokain/", statt der Normalen localhost Seite auf, bekommen Sie trotzdem die Normale localhost Seite zu sehen.
+// Steuern Sie jedoch direkt Datein an werden Ihnen diese statt der Main Seite angezeigt.
 app.get('/*', function (req, res) {
     fs.readFile('src/index.html', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
@@ -34,6 +35,7 @@ app.get('/*', function (req, res) {
 
 // Dieser Part starter nun den Server den wir eben etwas konfiguriert haben...
 app.listen(666, function () {
+    // Die Terminal Link funktion ist nur dafür da einen Link im Terminal Fenster bereit zu stellen, damit das alles etwas schöner wirkt.
     console.log('Die Webseite sollte sich nun automatisch auf machen \nFalls nicht, ' + terminalLink('klicken Sie hier...', 'https://localhost:666/'));
 });
 
