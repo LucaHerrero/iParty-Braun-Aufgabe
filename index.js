28dba60d62f290ccbcaf429d71e35736641e2a24
Luca Herrero <lucaherreromarcos@gmail.com>
2020-05-20 11:37:38 +0200

    Terminal etwas verschönert

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const open = require('open');
 // Dieses Paket dient dazu, falls das "open" Paket versagt, dass Sie in Ihrem Terminal auf ein Link klicken können...
 // Und nicht wie Steinzeit Menschen es mühsam abtippen müssen.
 const terminalLink = require('terminal-link');
+// Man brauchts nicht, aber ich fands lustig
+var figlet = require('figlet');
 
 // Weiterleitung und Fregabe der benötigten Verzeichnisse von Ihrem PC.
 // Mit dieser Methode kann man z.B. Verzeichnisse verlinken die es garnicht gibt.
@@ -20,7 +22,7 @@ app.use('/css', express.static(__dirname + '/node_modules/framework7/css'));
 // Im ersten Abschnitt müssen wir jedoch jede Seite definieren die ein eigenes nicht existente Unterverzeichis haben soll.
 // In diesem Beispiel benutzt für eine Funktion, für das versenden von Mails.
 app.get('/test', function (req, res) {
-    res.send('Hello World!');
+    res.send('Mail gesendet!');
 });
 // Hier werden alle Unterverzeichnisse, außer das oben genannte auf die Main index.html datei verlinkt, egal was sie aufrufen Sie kommen immer nur zur Startseite...
 // Kurzes Beispiel rufen Sie "http://localhost:666/ich-liebe-kokain/", statt der Normalen localhost Seite auf, bekommen Sie trotzdem die Normale localhost Seite zu sehen.
@@ -33,11 +35,23 @@ app.get('/*', function (req, res) {
     });
 });
 
+console.clear();
 // Dieser Part starter nun den Server den wir eben etwas konfiguriert haben...
 app.listen(666, function () {
     // Die Terminal Link funktion ist nur dafür da einen Link im Terminal Fenster bereit zu stellen, damit das alles etwas schöner wirkt.
     console.log('Die Webseite sollte sich nun automatisch auf machen \nFalls nicht, ' + terminalLink('klicken Sie hier...', 'https://localhost:666/'));
 });
-
 // Weil wir natürlich nicht in der Steinzeit leben, sollte dieser Befehl AUTOMATISCH ein Browserfenster öffnen.
 open('http://localhost:666/');
+
+// Den Part einfach ignorieren...
+figlet('iParties', function(err, data) {
+    if (err) {
+        console.log('iParties, für den täglichen Kokain Genuss!');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+    console.log('\nUnsere Empfehlung, drücken Sie [STRG/CTRL] in kombination mit der Taste [C],\ndanach nur noch bestätigen und die Performance der Webseite wird erhöht.\n');
+});
+// Ab jetzt wird's wieder interessant
