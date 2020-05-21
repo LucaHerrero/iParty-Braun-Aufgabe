7758e36ef007aff71f1f8f830caa200527f5bf76
Luca Herrero <lucaherreromarcos@gmail.com>
2020-05-21 13:48:03 +0200

    Impressum und Kontakt Seite ✔

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ var app = new Framework7({
   // Unterseiten Pfade
   routes: routes,
   view: {
-    pushState:true,
+    pushState: true,
     pushStateRoot: 'http://localhost:666',
     pushStateSeparator: ""
   }
@@ -17,50 +17,52 @@ var app = new Framework7({
 const $ = Dom7;
 var mainView = app.views.create('.view-main');
 
-
-app.request({
-  url: '/fileTree/pathID/0/fileURL/0',
-  method: 'GET',
-  dataType: 'json',
-  timeout: 30000,
-  success: function (data, status, xhr) {
-    for (let i of data) {
-      $("#impressionImages").append(
-        '<div class="swiper-slide">' +
-        '<img src="' + i + '" alt="ipression Image" class="impImages">' +
-        '</div>'
-      );
-    };
-    let swiper = app.swiper.create('.swiper-container', {
-      slidesPerView: 'auto',
-      spaceBetween: 10,
-      centeredSlides: false,
-      pagination: {
-        'el': '.swiper-pagination'
-      }
-    });
-  }
-});
-/* Man könnte noch Events wie Error oder so einbauen aber das wäre glaube etwas too much
-   Im normalerweise werden diese Daten von einer Datenbank geladen... in diesen Fall tun wir eindach so als wäre die news.json die Ausgabe einer Datenbank abfrage
-   Läd den Inhalt des News Bereichs aus einer Json datei */
-app.request({
-  url: '/js/news.json',
-  method: 'GET',
-  dataType: 'json',
-  timeout: 30000,
-  success: function (data, status, xhr) {
-    for (let i of data) {
-      $("#news-row").append(
-        '<div class="card card-header-pic col-100 medium-45">' +
-        '<div style="background-image:url(' + i.image + ')" class="card-header align-items-flex-end">' + i.title + '</div>' +
-        '<div class="card-content card-content-padding">' +
-        '<p class="date">Posted on ' + i.date + '</p>' +
-        '<p>' + i.long_text + '</p>' +
-        '</div>' +
-        '<div class="card-footer"><a href="#" class="link">Like ❤</a></div>' +
-        '</div>'
-      );
-    };
-  }
-});
+function loadContentIndex() {
+  app.request({
+    url: '/fileTree/pathID/0/fileURL/0',
+    method: 'GET',
+    dataType: 'json',
+    timeout: 30000,
+    success: function (data, status, xhr) {
+      for (let i of data) {
+        $("#impressionImages").append(
+          '<div class="swiper-slide">' +
+          '<img src="' + i + '" alt="ipression Image" class="impImages">' +
+          '</div>'
+        );
+      };
+      let swiper = app.swiper.create('.swiper-container', {
+        slidesPerView: 'auto',
+        spaceBetween: 10,
+        centeredSlides: false,
+        pagination: {
+          'el': '.swiper-pagination'
+        }
+      });
+    }
+  });
+  /* Man könnte noch Events wie Error oder so einbauen aber das wäre glaube etwas too much
+     Im normalerweise werden diese Daten von einer Datenbank geladen... in diesen Fall tun wir eindach so als wäre die news.json die Ausgabe einer Datenbank abfrage
+     Läd den Inhalt des News Bereichs aus einer Json datei */
+  app.request({
+    url: '/js/news.json',
+    method: 'GET',
+    dataType: 'json',
+    timeout: 30000,
+    success: function (data, status, xhr) {
+      for (let i of data) {
+        $("#news-row").append(
+          '<div class="card card-header-pic col-100 medium-45">' +
+          '<div style="background-image:url(' + i.image + ')" class="card-header news-header-text align-items-flex-end">' + i.title + '</div>' +
+          '<div class="card-content card-content-padding">' +
+          '<p class="date">Posted on ' + i.date + '</p>' +
+          '<p>' + i.long_text + '</p>' +
+          '</div>' +
+          '<div class="card-footer"><a href="#" class="link">Like ❤</a></div>' +
+          '</div>'
+        );
+      };
+    }
+  });
+};
+loadContentIndex();
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,7 +1,26 @@
+var indexLoadBug = 0;
 const routes = [{
         path: '/',
         url: '/index.html',
         name: 'home',
+        on: {
+            pageBeforeIn: function () {
+                if (indexLoadBug == 0) {
+                    loadContentIndex();
+                }
+                indexLoadBug++;
+            }
+        }
+    },
+    {
+        path: '/contact',
+        url: '/pages/contact.html',
+        name: 'contact',
+    },
+    {
+        path: '/impressum',
+        url: '/pages/impressum.html',
+        name: 'impressum',
     },
     {
         path: '(.*)',
